177347a6f0eea1923e53c21c05e5e5a962c8a879
Jaitank <tankjai123@gmail.com>
2022-07-22 12:39:28 +0530

    optimised approach of set matrix zero

--- 01_Array-01/01_Set_Matrix-Zero.cpp
+++ 01_Array-01/01_Set_Matrix-Zero.cpp
@@ -42,6 +42,42 @@ public:
     }
 };
 
+// Now Second solution without space (Optimised solution)
+
+// check strivers video to see its explanation
+
+class Solution2 {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        
+        int col0 = true;
+        int row = matrix.size();
+        int col = matrix[0].size();
+        
+        for(int i = 0; i < row; i++)
+        {
+            if(matrix[i][0] == 0) col0 = false;
+            for(int j = 1; j < col; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
+            }
+        }
+        // now change in matrix
+        for(int i = row - 1; i >= 0; i--)
+        {
+            for(int j = col - 1; j >= 1; j--)
+            {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+            if(col0 == false) matrix[i][0] = 0;
+        }
+        
+    }
+};
 
 
 int main()
